cf2a24589ca4542bc88902385828006a8d475ac4
Noah M Scott <124309801+Noah-M-Scott@users.noreply.github.com>
2025-07-07 23:10:31 -0500

    Update proto_produce.py

--- proto_produce.py
+++ proto_produce.py
@@ -16,7 +16,7 @@ HOST = '127.0.0.1'  # Server host
 PORT = 65432        # Server port to listen on
 
 # --- Producer (Type A) Constants ---
-NUM_PRODUCERS = 4  # The number of producer threads to create.
+NUM_PRODUCERS = 8  # The number of producer threads to create.
 NODE_NUMEBER = 0   # Index of the current node
 
 # Array of trace file names. Must have at least NUM_PRODUCERS elements.
@@ -24,7 +24,7 @@ TRACE_FILES = [f"traces/trace_{i}.trace" for i in range(NUM_PRODUCERS)]
 N_MICROSECONDS = 10    # Interval to read from trace file (This is swapped out for a file defined per line latency)
 M_MICROSECONDS = 50    # Interval to process data and add to log
 K_ITEMS = 131072       # Number of items in the temporary list before sending.
-RUN_TIMES = 3          # Number of times to rerun the trace
+RUN_TIMES = 100          # Number of times to rerun the trace
 
 # ==============================================================================
 #  Pricing Function
